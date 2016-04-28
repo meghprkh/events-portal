92b84139c1e86764524096abef7ea2b3b929a445
Megh Parikh <meghprkh@gmail.com>
2016-04-28 22:51:16 +0530

    Get rid of views

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var passport = require('passport');
 
 require('./passport_setup');
 
-app.set('views', 'views');
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
