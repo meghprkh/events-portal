f540edb49d011b3b62c1f25068c6666fb3fdf192
Megh Parikh <meghprkh@gmail.com>
2016-04-30 10:18:31 +0530

    Add logger to redux in the dev environment

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,21 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
-)(createStore)
+let createStoreWithMiddleware;
+
+if (process.env.NODE_ENV === 'production') {
+  createStoreWithMiddleware = applyMiddleware(
+    thunkMiddleware
+  )(createStore)
+} else {
+  const loggerMiddleware = createLogger()
+  createStoreWithMiddleware = applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )(createStore)
+}
 
 function configureStore(initialState) {
   let store = createStoreWithMiddleware(rootReducer, initialState)
