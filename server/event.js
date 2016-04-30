6e1ec46ae57a3a1f791edb3554e2e461f02b8c4f
Megh Parikh <meghprkh@gmail.com>
2016-04-30 12:41:29 +0530

    Add going status to event API response

--- server/event.js
+++ server/event.js
@@ -4,11 +4,37 @@ var middleware = require('./middleware')
 require('./route-params.js')(router)
 
 router.get('/', (req, res) => {
-  models.Event.findAll().then(events => res.send(events));
+  if (req.user && req.user.type == 'User') {
+    models.Event.findAll({
+      include: [{
+        model: models.User,
+        where: {id: req.user.id},
+        required: false,
+        attributes: ['id']
+      }]
+    }).then(es => {
+      var events = es.map(e => {
+        var event = e.toJSON()
+        if (event.users.length) event.going = true;
+        else event.going = false;
+        event.users = undefined;
+        return event
+      })
+      res.send(events)
+    });
+  }
+  else models.Event.findAll().then(events => res.send(events));
 })
 
 router.get('/:event_id', (req, res) => {
-  res.send(req.pevent)
+  var event = req.pevent.toJSON();
+  if (req.user && req.user.type == 'User') {
+    req.pevent.hasUser(req.user).then(going => {
+      event.going = going;
+      res.send(event)
+    })
+  }
+  else res.send(event)
 })
 
 router.get('/:event_id/going', middleware.isUser, (req, res) => {
